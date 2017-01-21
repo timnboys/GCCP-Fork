076577ddb4d2376ca4d9de3b492319d452fc1986
timnboys <timnboys@gmail.com>
2017-01-20 21:11:03 -0500

    Revert "Revert "applications-module""
    This reverts commit 46f4d7860f3886d33ce791c1dfd4741ccdc00bd7.

---
+++ Modules/Applications/Database/Seeders/ApplicationsDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Applications\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicationsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
