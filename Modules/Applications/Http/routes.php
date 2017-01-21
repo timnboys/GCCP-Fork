46f4d7860f3886d33ce791c1dfd4741ccdc00bd7
timnboys <timnboys@gmail.com>
2017-01-20 20:55:14 -0500

    Revert "applications-module"
    This reverts commit 8b9a8dc3a996a53070b65f1a3e2159ebe291f996.

--- Modules/Applications/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'applications', 'namespace' => 'Modules\Applications\Http\Controllers'], function()
-{
-    Route::get('/', 'ApplicationsController@index');
-});
--- Modules/Applications/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('applications::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('applications.name') !!}
-    </p>
-@stop
